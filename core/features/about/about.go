f3f32c7d4e984e9641e3ea2249efb1f8c8aec4f6
noreply <noreply@yourdomain.com>
2024-08-14 11:39:13 +0800

    v0.2.2 debug

--- core/features/about/about.go
+++ core/features/about/about.go
@@ -3,22 +3,24 @@ package about
 import (
 	"github.com/AnnonaOrg/annona_bot/core/constvar"
 	"github.com/AnnonaOrg/annona_bot/core/features"
-	"github.com/AnnonaOrg/osenv"
+
+	// "github.com/AnnonaOrg/osenv"
 	tele "gopkg.in/telebot.v3"
 )
 
 func init() {
-	features.RegisterFeature("/about", OnProcess)
+	// features.RegisterFeature("/about", OnProcess)
 	features.RegisterFeature("/version", OnVersion)
 }
-func OnProcess(c tele.Context) error {
-	if !osenv.IsBotManagerID(c.Message().Sender.ID) {
-		return nil
-	}
-	text := constvar.APPAbout()
-	return c.Reply(text)
-}
+
+//	func OnProcess(c tele.Context) error {
+//		if !osenv.IsBotManagerID(c.Message().Sender.ID) {
+//			return nil
+//		}
+//		text := constvar.APPAbout()
+//		return c.Reply(text)
+//	}
 func OnVersion(c tele.Context) error {
-	text := constvar.APPVersion()
+	text := constvar.APPAbout()
 	return c.Reply(text)
 }
--- core/features/ping/ping.go
+++ core/features/ping/ping.go
@@ -1,8 +1,6 @@
 package ping
 
 import (
-	"fmt"
-
 	"github.com/AnnonaOrg/annona_bot/core/features"
 	tele "gopkg.in/telebot.v3"
 )
@@ -15,12 +13,6 @@ func OnPing(c tele.Context) error {
 	if !c.Message().Private() {
 		return nil
 	}
-	text := fmt.Sprintf("Pong! %s%s \n@%s(%d)",
-		c.Message().Sender.FirstName, c.Message().Sender.LastName,
-		c.Message().Sender.Username, c.Message().Sender.ID,
-	)
 
-	// c.Delete()
-	return c.Reply(text)
-	// return c.Send(text)
+	return c.Reply("Pong!")
 }
