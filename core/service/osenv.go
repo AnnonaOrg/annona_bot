6435294a0bd6d23fdd261351f3b39365f7cd9d6e
noreply <noreply@yourdomain.com>
2024-11-19 11:11:06 +0800

    v0.2.3 debug

--- core/service/osenv.go
+++ core/service/osenv.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/AnnonaOrg/osenv"
@@ -21,4 +22,12 @@ func IsRetryPushMsgEnable() bool {
 func IsEnableFilterSameSenderUserMsg() bool {
 	// Filter messages triggered multiple times by the same user within a short period of time
 	return strings.EqualFold(osenv.Getenv("FILTER_SAME_SENDER_USER_MSG_ENABLE"), "true")
+}
+func GetMinCountFilterSameSenderUserMsg() int {
+	item := osenv.Getenv("FILTER_SAME_SENDER_USER_MSG_MINI_COUNT")
+	count, err := strconv.Atoi(item)
+	if err != nil || count == 0 {
+		return 500
+	}
+	return count
 }
