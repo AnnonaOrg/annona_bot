e6dd49cf1b93db0065f39ffb0191a50fc9cfcbba
noreply <noreply@yourdomain.com>
2024-11-19 10:41:47 +0800

    v0.2.3 debug

--- core/service/osenv.go
+++ core/service/osenv.go
@@ -17,3 +17,8 @@ func GetURLSubmitNewGroup() string {
 func IsRetryPushMsgEnable() bool {
 	return strings.EqualFold(osenv.Getenv("RETRY_PUSH_MSG_ENABLE"), "true")
 }
+
+func IsEnableFilterSameSenderUserMsg() bool {
+	// Filter messages triggered multiple times by the same user within a short period of time
+	return strings.EqualFold(osenv.Getenv("FILTER_SAME_SENDER_USER_MSG_ENABLE"), "true")
+}
